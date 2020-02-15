86033fafebdb6ce8963acf0f87b7d6c7cf5e9042
Rishab Manocha <rishab.manocha@outlook.com>
2020-02-15 15:46:10 -0800

    NPM private registry. Index File for cleaner exports.
    Added private registry to which package can be published.
    The index file allows for cleaner imports for the other
    projects

---
+++ index.js
@@ -0,0 +1,11 @@
+const { Whatsapp } = require("./src/whatsapp");
+const { WhatsappAction } = require("./src/whatsappAction");
+const { XPATH, NAME_PLACEHOLDER } = require("./src/constants");
+const { Driver } = require("./src/driver");
+
+
+module.exports = {
+    Whatsapp, WhatsappAction,
+    XPATH, NAME_PLACEHOLDER,
+    Driver
+}
